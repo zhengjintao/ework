1b30e6c5c073ecab90546b97982f7ede40b23ba6
fangkangming <fangkm1021@163.com>
2018-02-05 14:53:21 +0900

    servlet add
    servlet add

--- src/main/java/com/bwc/ework/servlets/LeaveServlet.java
+++ src/main/java/com/bwc/ework/servlets/LeaveServlet.java
@@ -1,27 +1,21 @@
 package com.bwc.ework.servlets;
 
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
-
-import com.bwc.ework.common.JdbcUtil;
 
 /**
- * Servlet implementation class AddServlet
+ * Servlet implementation class leaveServlte
  */
-public class AddWorkDayServlet extends HttpServlet {
+public class LeaveServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AddWorkDayServlet() {
+    public LeaveServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -30,26 +24,8 @@ public class AddWorkDayServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String date = request.getParameter("wdate");
-		String begin = request.getParameter("wbegin");
-		String end = request.getParameter("wend");
-		
-		HttpSession session = request.getSession();
-		String userId = (String)session.getAttribute("userId");
-		
-		if(userId == null || userId.length() == 0){
-			response.sendRedirect("login.jsp");
-			return;
-		}
-		
-		JdbcUtil  jdbc = new JdbcUtil();
-		String sql = "select * from mstr_user";
-		List<Object> list = jdbc.excuteQuery(sql,null);
-		
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(date);
-		response.getWriter().append("Ser  ved at: ").append(begin);
-		response.getWriter().append("Served at: ").append(end);
+		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
@@ -58,8 +34,6 @@ public class AddWorkDayServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
-		
-
 	}
 
 }
--- src/main/java/com/bwc/ework/servlets/ListServlet.java
+++ src/main/java/com/bwc/ework/servlets/ListServlet.java
@@ -5,18 +5,17 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
- * Servlet implementation class AddLeaveDayServlet
+ * Servlet implementation class ListServlet
  */
-public class AddLeaveDayServlet extends HttpServlet {
+public class ListServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public AddLeaveDayServlet() {
+    public ListServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
@@ -25,22 +24,8 @@ public class AddLeaveDayServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String date = request.getParameter("wdate");
-		String begin = request.getParameter("wbegin");
-		String end = request.getParameter("wend");
-		
-		HttpSession session = request.getSession();
-		String userId = (String)session.getAttribute("userId");
-		
-		if(userId == null || userId.length() == 0){
-			response.sendRedirect("login.jsp");
-			return;
-		}
-		
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(date);
-		response.getWriter().append("Served at: ").append(begin);
-		response.getWriter().append("Served at: ").append(end);
+		response.sendRedirect("list.jsp");
 	}
 
 	/**
