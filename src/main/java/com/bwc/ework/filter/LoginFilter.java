d6e705de74180ca575d92cdbc5195444465a5c29
zhengjintao <xiaonei0912@qq.com>
2018-02-05 21:37:54 +0900

    index servlet made

--- src/main/java/com/bwc/ework/filter/LoginFilter.java
+++ src/main/java/com/bwc/ework/filter/LoginFilter.java
@@ -11,8 +11,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.bwc.ework.form.User;
-
 /**
  * Servlet Filter implementation class LoginFilter
  */
