b5684b8b34b38ca7a7a1ef87b4837856519f90bc
fangkangming <fangkm1021@163.com>
2018-02-05 20:31:58 +0900

    stringToDate
    stringToDate

--- src/main/java/com/bwc/ework/common/DateTimeUtil.java
+++ src/main/java/com/bwc/ework/common/DateTimeUtil.java
@@ -1,25 +1,47 @@
 package com.bwc.ework.common;
 
 import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class DateTimeUtil {
-	public String formatTime(Time time){
+	public static String formatTime(Time time) {
 		int hour = time.getHours();
-        int minute = time.getMinutes();
-        String hourString;
-        String minuteString;
+		int minute = time.getMinutes();
+		String hourString;
+		String minuteString;
 
-        if (hour < 10) {
-            hourString = "0" + hour;
-        } else {
-            hourString = Integer.toString(hour);
-        }
-        if (minute < 10) {
-            minuteString = "0" + minute;
-        } else {
-            minuteString = Integer.toString(minute);
-        }
-        return (hourString + ":" + minuteString);
+		if (hour < 10) {
+			hourString = "0" + hour;
+		} else {
+			hourString = Integer.toString(hour);
+		}
+		if (minute < 10) {
+			minuteString = "0" + minute;
+		} else {
+			minuteString = Integer.toString(minute);
+		}
+		return (hourString + ":" + minuteString);
 	}
 
+	public static Date stringToDate(String str) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		try {
+			return sdf.parse(str);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	}
+
+	public static Time stringToTime(String str) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		try {
+			return new Time(sdf.parse(str).getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			return null;
+		}
+	}
 }
