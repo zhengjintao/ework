fc745a623b8c292fc218a006212aa55eabc03142
fangkangming <fangkm1021@163.com>
2018-02-24 10:00:57 +0100

    出勤修正
     出勤修正

--- src/main/java/com/bwc/ework/servlets/ListServlet.java
+++ src/main/java/com/bwc/ework/servlets/ListServlet.java
@@ -1,10 +1,11 @@
 ﻿package com.bwc.ework.servlets;
 
 import java.io.IOException;
-import java.sql.Time;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +16,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import com.alibaba.fastjson.JSONObject;
 import com.bwc.ework.common.DateTimeUtil;
 import com.bwc.ework.common.JdbcUtil;
 import com.bwc.ework.form.User;
@@ -39,8 +41,12 @@ public class ListServlet extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		User userinfo = (User)session.getAttribute("userinfo");
-	    
-		// 
+		request.setAttribute("qiandao", "签到");
+		if("1".equals(request.getParameter("deleteFlg"))){
+			delete(request,response);
+			return;
+		}
+		// 初期化的场合
 		if("true".equals(request.getParameter("subKbn")) && !"1".equals(request.getParameter("selectChg"))){
 			String sql = "select * from cdata_worktime where userid=? and date=?";
 			Object[] params = new Object[2];
@@ -90,6 +96,13 @@ public class ListServlet extends HttpServlet {
 			request.setAttribute("defaultBeginTime",begin);
 			// 默认结束时间
 			request.setAttribute("defaultEndTime", end);
+			request.setAttribute("qiandao", "已签");
+			try {
+				getWeekData(request);
+			} catch (ParseException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 		}else{
 			// 系统当前时间取得
 			SimpleDateFormat formattime=new SimpleDateFormat("yyyy-MM-dd"); 
@@ -104,20 +117,69 @@ public class ListServlet extends HttpServlet {
 			params[1] = dateTime;
 			List<Object> list1 = JdbcUtil.getInstance().excuteQuery(sql, params);
 			
-			if(list1.size()>0){
-				Map<String, Object> set = (Map<String, Object>)list1.get(0);
-				// 默认开始时间
-				request.setAttribute("defaultBeginTime",set.get("begintime").toString());
-				// 默认结束时间
-				request.setAttribute("defaultEndTime", set.get("endtime").toString());
-			}else{
-				// 默认开始时间
-				request.setAttribute("defaultBeginTime",userinfo.getBeginTime().toString());
-				// 默认结束时间
-				request.setAttribute("defaultEndTime", userinfo.getEndTime().toString());
+			if("1".equals(request.getParameter("selectChg"))){
+				try {
+					getWeekData(request);
+				} catch (ParseException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+				
+				JSONObject jsonObject = new JSONObject();
+				if(list1.size()>0){
+					Map<String, Object> set = (Map<String, Object>)list1.get(0);
+					jsonObject.put("defaultBeginTime", set.get("begintime").toString()); 
+					jsonObject.put("defaultEndTime", set.get("endtime").toString());
+					jsonObject.put("settedFlg", "1");
+				}else{
+					jsonObject.put("defaultBeginTime",userinfo.getBeginTime().toString());
+					jsonObject.put("defaultEndTime", userinfo.getEndTime().toString());
+					jsonObject.put("settedFlg", "0");
+				}
+				jsonObject.put("week0", request.getAttribute("week0"));
+				jsonObject.put("week1", request.getAttribute("week1"));
+				jsonObject.put("week2", request.getAttribute("week2"));
+				jsonObject.put("week3", request.getAttribute("week3"));
+				jsonObject.put("week4", request.getAttribute("week4"));
+				jsonObject.put("week5", request.getAttribute("week5"));
+				jsonObject.put("week6", request.getAttribute("week6"));
+		        response.setCharacterEncoding("utf-8");  
+		        response.getWriter().write(jsonObject.toString());
+
+		        return;
+			}
+			else{
+				if(list1.size()>0){
+					Map<String, Object> set = (Map<String, Object>)list1.get(0);
+					// 默认开始时间
+					request.setAttribute("defaultBeginTime",set.get("begintime").toString());
+					// 默认结束时间
+					request.setAttribute("defaultEndTime", set.get("endtime").toString());
+					
+					request.setAttribute("qiandao", "已签");
+					try {
+						getWeekData(request);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}else{
+					// 默认开始时间
+					request.setAttribute("defaultBeginTime",userinfo.getBeginTime().toString());
+					// 默认结束时间
+					request.setAttribute("defaultEndTime", userinfo.getEndTime().toString());
+					
+					request.setAttribute("qiandao", "签到");
+					try {
+						getWeekData(request);
+					} catch (ParseException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
 			}
 		}
-		
+  
 		RequestDispatcher re = request.getRequestDispatcher("list.jsp");
 		re.forward(request, response);
 	}
@@ -129,5 +191,127 @@ public class ListServlet extends HttpServlet {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
+	
+	
+	private void getWeekData(HttpServletRequest request) throws ParseException{
+		HttpSession session = request.getSession();
+		User userinfo = (User)session.getAttribute("userinfo");
+		String dateStr = request.getParameter("wdate2");
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		List<String> weekDate = convertWeekDate(dateStr == null ? new Date() : sdf.parse(dateStr));
+		
+		String dispStr;
+		for(int i= 0; i<7;i++){
+			dispStr = weekDate.get(i);
+			String sql = "select * from cdata_worktime where userid=? and date = ?";
+			Object[] params = new Object[2];
+			params[0] = userinfo.getUserId();
+			params[1] = weekDate.get(i);
+			List<Object> list1 = JdbcUtil.getInstance().excuteQuery(sql, params);
+			if(list1.size() > 0){
+				Map<String, Object> set = (Map<String, Object>)list1.get(0);
+				dispStr = dispStr + "&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp" + set.get("begintime").toString()
+						+ "&nbsp&nbsp" + "~" + "&nbsp&nbsp" + set.get("endtime").toString();
+			}
+			
+			request.setAttribute("week" + i , dispStr);
+		}
+	}
+	
+	/**
+	 * 根据选择日期取得本周的所有日期
+	 * @param time
+	 * @return
+	 */
+	private List<String> convertWeekDate(Date time) {  
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd"); //设置时间格式  
+        Calendar cal = Calendar.getInstance();  
+        cal.setTime(time);  
+        
+        //判断要计算的日期是否是周日，如果是则减一天计算周六的，否则会出问题，计算到下一周去了  
+        int dayWeek = cal.get(Calendar.DAY_OF_WEEK);//获得当前日期是一个星期的第几天  
+        if(1 == dayWeek) {  
+          cal.add(Calendar.DAY_OF_MONTH, -1);  
+        }  
+        cal.setFirstDayOfWeek(Calendar.MONDAY);//设置一个星期的第一天，按中国的习惯一个星期的第一天是星期一  
+        int day = cal.get(Calendar.DAY_OF_WEEK);//获得当前日期是一个星期的第几天  
+        cal.add(Calendar.DATE, cal.getFirstDayOfWeek()-day);//根据日历的规则，给当前日期减去星期几与一个星期第一天的差值   
+        String beginDate = sdf.format(cal.getTime()); //周一时间   
+        List<String> result = new ArrayList<String>();
+        result.add(beginDate + "&nbsp&nbsp"+ getweekDay(cal));
+        for(int i=1;i<7;i++){
+        	cal.add(Calendar.DATE,1);
+        	result.add(sdf.format(cal.getTime()) +"&nbsp&nbsp"+ getweekDay(cal));
+        }
+        
+        return result;
+   } 
+	
+	/**
+	 * 根据日期取得星期几
+	 * @param cal
+	 * @return
+	 */
+	private String getweekDay(Calendar cal){
+		String str = "";
+		switch (cal.get(Calendar.DAY_OF_WEEK)){
+		    case Calendar.MONDAY:
+		    	str = "(一)";
+		    	break;
+		    case Calendar.TUESDAY:
+		    	str = "(二)";
+		    	break;
+		    case Calendar.WEDNESDAY:
+		    	str = "(三)";
+		    	break;
+		    case Calendar.THURSDAY:
+		    	str = "(四)";
+		    	break;
+		    case Calendar.FRIDAY:
+		    	str = "(五)";
+		    	break;
+		    case Calendar.SATURDAY:
+		    	str = "(六)";
+		    	break;
+		    case Calendar.SUNDAY:
+		    	str = "(日)";
+		    	break;
+		}
+		
+		return str;
+	}
+	
+	private void delete(HttpServletRequest request,HttpServletResponse response) throws IOException{
+		HttpSession session = request.getSession();
+		User userinfo = (User)session.getAttribute("userinfo");
+		
+		String sql = "delete from cdata_worktime where userid=? and date=?";
+		Object[] params = new Object[2];
+		params[0] = userinfo.getUserId();
+		params[1] = request.getParameter("wdate");
+		JdbcUtil.getInstance().executeUpdate(sql, params);
+		
+		JSONObject jsonObject = new JSONObject();
+		try {
+			getWeekData(request);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		jsonObject.put("defaultBeginTime",userinfo.getBeginTime().toString());
+		jsonObject.put("defaultEndTime", userinfo.getEndTime().toString());
+		jsonObject.put("settedFlg", "0");
+		jsonObject.put("week0", request.getAttribute("week0"));
+		jsonObject.put("week1", request.getAttribute("week1"));
+		jsonObject.put("week2", request.getAttribute("week2"));
+		jsonObject.put("week3", request.getAttribute("week3"));
+		jsonObject.put("week4", request.getAttribute("week4"));
+		jsonObject.put("week5", request.getAttribute("week5"));
+		jsonObject.put("week6", request.getAttribute("week6"));
+        response.setCharacterEncoding("utf-8");  
+        response.getWriter().write(jsonObject.toString());
 
+        return;
+	}
 }
--- src/main/webapp/list.jsp
+++ src/main/webapp/list.jsp
@@ -14,6 +14,55 @@
 <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 <script src="dist/components/form.js"></script>
 <script src="dist/components/transition.js"></script>
+<script type="text/javascript">
+function getSettedtime()
+{
+	$.ajax({ 
+	    type: "post", 
+	    url: "./list.do?" + $("form").serialize() + "&selectChg=1", 
+	    dataType: "json", 
+	    success: function (data) {
+	    	$("#wbegin").val(data.defaultBeginTime)
+	    	$("#wend").val(data.defaultEndTime)
+	    	$("#actionname").html("1" == data.settedFlg ? "已签" : "签到")
+	    	$("#week0").html(data.week0)
+	    	$("#week1").html(data.week1)
+	    	$("#week2").html(data.week2)
+	    	$("#week3").html(data.week3)
+	    	$("#week4").html(data.week4)
+	    	$("#week5").html(data.week5)
+	    	$("#week6").html(data.week6)
+	    }, 
+	    error: function() {
+	            alert("网络异常，请稍后重试");
+	    } 
+	});
+}
+
+function deleteData()
+{
+	$.ajax({ 
+	    type: "post", 
+	    url: "./list.do?" + $("form").serialize() + "&deleteFlg=1", 
+	    dataType: "json", 
+	    success: function (data) {
+	    	$("#wbegin").val(data.defaultBeginTime)
+	    	$("#wend").val(data.defaultEndTime)
+	    	$("#actionname").html("1" == data.settedFlg ? "已签" : "签到")
+	    	$("#week0").html(data.week0)
+	    	$("#week1").html(data.week1)
+	    	$("#week2").html(data.week2)
+	    	$("#week3").html(data.week3)
+	    	$("#week4").html(data.week4)
+	    	$("#week5").html(data.week5)
+	    	$("#week6").html(data.week6)
+	    }, 
+	    error: function() {
+            alert("网络异常，请稍后重试");
+	    } 
+	});
+}
+</script>
 
 <style type="text/css">
 body {
@@ -41,14 +90,14 @@ footer {
 					<div class="ui inverted form">
 						<div class="inline field">
 							<div class="field">
-								<label>日期</label> <input type="date" name="wdate"
+								<label>日期</label> <input type="date" name="wdate" onchange="getSettedtime()"
 									value=<%=(String) request.getAttribute("sysDate")%>>
 							</div>
 						</div>
 						<div class="two fields">
 							<div class="one fields">
 								<div class="field">
-									<label>出勤时间</label> <input type="time" name="wbegin"
+									<label>出勤时间</label> <input type="time" name="wbegin" id="wbegin"
 										value=<%=(String) request.getAttribute("defaultBeginTime")%>><br>
 								</div>
 
@@ -57,40 +106,50 @@ footer {
 								</div>
 
 								<div class="field">
-									<label>退勤时间</label> <input type="time" name="wend"
+									<label>退勤时间</label> <input type="time" name="wend" id="wend"
 										value=<%=(String) request.getAttribute("defaultEndTime")%>><br>
 								</div>
 							</div>
 						</div>
 						<input type="hidden" name="subKbn" value="true"><br>
-						<Button class="ui active teal button" type="submit">
-							<i class="add to calendar icon"></i> 签到
+						<Button class="ui active teal button">
+							<i class="add to calendar icon"></i> <span id="actionname"><%=(String) request.getAttribute("qiandao")%></span>
 						</Button>
+						<div class="ui active teal button" onclick = "deleteData()" >
+							<i class="trash icon"></i> 删除
+						</div>
 					</div>
 				</div>
-			</form>
 			<div class="ui grey inverted segment">
 				<a class="ui orange right ribbon label">当周出勤</a> <input type="date"
-					name="wdate" value="2018-02-01">
+					name="wdate2" value=<%=(String) request.getAttribute("sysDate")%> onchange="getSettedtime()">
 				<table class="ui celled table">
 					<tbody>
 						<tr>
-							<td>2018/1/2 09:00 ~ 17:30</td>
+							<td id= "week0"><%=(String) request.getAttribute("week0")%></td>
 						</tr>
 						<tr>
-							<td>2018/1/2 09:00 ~ 17:30</td>
+							<td id= "week1"><%=(String) request.getAttribute("week1")%></td>
 						</tr>
 						<tr>
-							<td>2018/1/2 09:00 ~ 17:30</td>
+							<td id= "week2"><%=(String) request.getAttribute("week2")%></td>
 						</tr>
 						<tr>
-							<td>2018/1/2 09:00 ~ 17:30</td>
+							<td id= "week3"><%=(String) request.getAttribute("week3")%></td>
+						</tr>
+						<tr>
+							<td id= "week4"><%=(String) request.getAttribute("week4")%></td>
+						</tr>
+						<tr>
+							<td id= "week5"><%=(String) request.getAttribute("week5")%></td>
+						</tr>
+						<tr>
+							<td id= "week6"><%=(String) request.getAttribute("week6")%></td>
 						</tr>
 					</tbody>
 				</table>
-
-
 			</div>
+		</form>
 		</div>
 
 	</div>
