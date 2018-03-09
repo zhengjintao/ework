b6b77be08d1b71eba4490d5a35adc3d221069931
fangkangming <fangkm1021@163.com>
2018-03-09 23:40:00 +0900

    复数请假日期
    复数请假日期

--- src/main/java/com/bwc/ework/servlets/LeaveServlet.java
+++ src/main/java/com/bwc/ework/servlets/LeaveServlet.java
@@ -37,11 +37,14 @@ public class LeaveServlet extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
 	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	protected void doGet1(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		User userinfo = (User) session.getAttribute("userinfo");
 		String wdate = request.getParameter("wdate");
+		
+		//请假时间取得（复数可）
+		String[] wdateList =request.getParameter("wdate").split(",");
 		String wdate2 = request.getParameter("wdate2");
 
 		// 系统当前时间取得
@@ -232,4 +235,162 @@ public class LeaveServlet extends HttpServlet {
 		params[1] = request.getParameter("wdate");
 		JdbcUtil.getInstance().executeUpdate(sql, params);
 	}
+	
+	
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		// 请假的开始日期
+		String wdate= request.getParameter("wdate");
+		// 用户信息
+		User userinfo = (User) session.getAttribute("userinfo");
+		// 请假时间（复数可）
+		String[] wdateList =wdate.split(",");
+
+		// 画面查询时间
+		String wdate2 = request.getParameter("wdate2");
+		// 系统当前时间取得
+		SimpleDateFormat formattime = new SimpleDateFormat("yyyy-MM-dd");
+		String now = formattime.format(new Date());
+		if (wdateList == null || wdateList.length == 0 || wdate2 == null || wdate2.length() == 0) {
+			wdate = now;
+			wdate2 = DateTimeUtil.GetMonth(now);
+		}
+
+		// 请假理由转码
+		String wcomment = request.getParameter("wcomment");
+		if (wcomment != null) {
+			wcomment = new String(wcomment.getBytes("iso-8859-1"), "utf-8");
+		}
+
+		String subkbn = request.getParameter("subKbn");
+		String oklabel = "确定";
+
+		// IN条件作成
+        StringBuffer buffer = new StringBuffer();  
+        for (int i = 0; i < wdateList.length; i++)  
+        {  
+            buffer.append("?, ");  
+        }  
+        buffer.deleteCharAt(buffer.length() - 1);  
+        buffer.deleteCharAt(buffer.length() - 1); 
+        
+		Object[] params = new Object[wdateList.length+1];
+		params[0] = userinfo.getUserId();
+        for (int i = 0; i < wdateList.length; i++)  
+        {  
+        	params[i+1] = wdateList[i]; 
+        }  
+        
+		// 初期化
+		if (subkbn == null) {
+			wdate = now;
+			wdate2 = DateTimeUtil.GetMonth(wdate);
+		} else {
+
+			// 删除
+			if ("1".equals(request.getParameter("deleteFlg"))) {
+				delete(request, response);
+			}
+
+			// 初期化的场合
+			if ("true".equals(request.getParameter("subKbn")) && !"1".equals(request.getParameter("selectChg"))  && !"1".equals(request.getParameter("leaveinfo"))
+					&& !"1".equals(request.getParameter("deleteFlg"))) {
+				String sql = "select * from cdata_worktime where userid=? and date in ("+ buffer.toString() +")";
+
+				List<Object> listw = JdbcUtil.getInstance().excuteQuery(sql, params);
+				if (listw.size() > 0) {
+					request.setAttribute("errmsg", "当天已签到，无法请假！");
+				} else {
+					// 删除
+					String deleteSql = "delete from cdata_leave where userid=? and leavedate in ("+ buffer.toString() +")";
+					Object[] delparams = new Object[2];
+					delparams[0] = userinfo.getUserId();
+					delparams[1] = wdate;
+					JdbcUtil.getInstance().executeUpdate(deleteSql, params);
+
+					// 数据存在更新操作
+					String insertSql = "insert into cdata_leave value(?,?,?,?,?)";
+					Object[] insertparams = new Object[5];
+					String userid = userinfo.getUserId();
+					for(int i=0;i<wdateList.length;i++){
+						com.bwc.ework.form.Date date1 = DateTimeUtil.stringToDate(wdateList[i]);			
+						insertparams[0] = userid;
+						insertparams[1] = wdateList[i];
+						insertparams[2] = date1.getYear();
+						insertparams[3] = date1.getMonth();
+						insertparams[4] = wcomment == null || wcomment.length() == 0 ? "未填写理由" : wcomment;
+						JdbcUtil.getInstance().executeUpdate(insertSql, insertparams);
+					}
+				}
+
+				wdate2 = DateTimeUtil.GetMonth(wdateList[0]);
+			}
+		}
+
+		// 日期设定
+		request.setAttribute("sysDate", wdateList[0]);
+		// 日期设定
+		request.setAttribute("sysDate2", wdate2);
+
+		String sql = "select * from cdata_leave where userid=? and leavedate in ("+ buffer.toString() +")";
+		List<Object> list1 = JdbcUtil.getInstance().excuteQuery(sql, params);
+		String comment = "";
+		if (list1.size() > 0) {
+			Map<String, Object> set = (Map<String, Object>) list1.get(0);
+			comment = set.get("content").toString();
+			request.setAttribute("wcomment", comment);
+			oklabel = "已请";
+		}
+
+		request.setAttribute("wcomment", comment);
+
+		if ("1".equals(request.getParameter("selectChg"))) {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("comment", comment);
+			jsonObject.put("oklabel", oklabel);
+			response.setCharacterEncoding("utf-8");
+			response.getWriter().write(jsonObject.toString());
+
+			return;
+		}
+		
+		List<String[]> monthinfo = getMonthData(userinfo.getUserId(), wdate2);
+		StringBuilder info= new StringBuilder();
+		if(monthinfo.size() == 0){
+			info.append("<tr>");
+			info.append("<td>");
+			info.append("当月没有请假");
+			info.append("</td>");
+			info.append("</tr>");
+		}
+		for(String[] each : monthinfo){
+			info.append("<tr>");
+			info.append("<td>");
+			info.append(each[0]);
+			info.append("</td>");
+			info.append("<td>");
+			info.append(each[1]);
+			info.append("</td>");
+			info.append("</tr>");
+		}
+		if ("1".equals(request.getParameter("leaveinfo"))) {
+			JSONObject jsonObject = new JSONObject();
+			jsonObject.put("info", info.toString());
+			response.setCharacterEncoding("utf-8");
+			response.getWriter().write(jsonObject.toString());
+
+			return;
+		}
+		
+		request.setAttribute("oklabel", oklabel);
+		request.setAttribute("info", info.toString());
+		RequestDispatcher re = request.getRequestDispatcher("leave.jsp");
+		re.forward(request, response);
+	}
+
 }
