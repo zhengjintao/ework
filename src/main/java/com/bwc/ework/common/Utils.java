8ffc51e9d13734e26a61a47f601f217f18a5b1eb
zhengjintao <xiaonei0912@qq.com>
2018-05-12 11:41:08 +0900

    取得最终存储用公司id

--- src/main/java/com/bwc/ework/common/Utils.java
+++ src/main/java/com/bwc/ework/common/Utils.java
@@ -4,5 +4,13 @@ public class Utils {
 	public static boolean isDefaultCompany(String companyid){
 		return companyid == null || companyid.length() == 0 || Consts.DefaultCompanyId.equals(companyid);
 	}
+	
+	public static String getStoreCompanyid(String companyid){
+		if(isDefaultCompany(companyid)){
+			return Consts.DefaultCompanyId;
+		}else{
+			return companyid;
+		}
+	}
 
 }
